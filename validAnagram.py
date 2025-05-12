b241b9be51996a3685fe0d0dfa8a14f04b8a5009
AshmiyaVC <avijayachandran@crowdplat.com>
2025-05-12 16:05:00 -0700

    valid anagram

---
+++ validAnagram.py
@@ -0,0 +1,23 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False;
+
+        countS, countT = {}, {}
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+
+        return True
+    
+s = "racecar"
+t = "carrace"
+print(Solution().isAnagram(s, t))
+
+s2 = "jar"
+t2 = "jam"
+print(Solution().isAnagram(s2, t2))
